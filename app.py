0c00401786a0b8b56f0c574e4d02674504040556
Mahad Osman <mahado14@gmail.com>
2022-11-24 12:20:46 -0500

    Step one complete

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 from boggle import Boggle
+from flask import Flask, request, session, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "test_secret123"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False #Removing the consistent redirect
+debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
+
+@app.route('/')
+def index():
+    return render_template("home.html")

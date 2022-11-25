bb6e2f7eedc28b737b1fc04063ae9eab2b945da0
Mahad Osman <mahado14@gmail.com>
2022-11-25 12:47:08 -0500

    Finish part_three

--- Static/JS/app.js
+++ Static/JS/app.js
@@ -1,11 +1,25 @@
 console.log("The File is loading!")
 
 const $guess_form = $(".guess-from")
+const $result = $(".result")
 
 async function handle_Submit(evt){
     evt.preventDefault();
     const $guess = $(".guess")
-    console.log($guess.val())
+    let guess = $guess.val()
+    //console.log($guess.val())
+
+    const res = await axios.get("/word_check", {params: {guess: guess}});
+    console.log(res.data)
+
+    if (res.data.response === "not-on-board"){
+        $result.text(`${guess} is not on the board`)
+    } else if(res.data.response === "not-word"){
+        $result.text(`${guess} is not a word`)
+    }else{
+
+    }
+
 }
 
 $guess_form.on("submit", handle_Submit)
--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 #from torch import BoolStorage
+from crypt import methods
+from inspect import BoundArguments
 from operator import methodcaller
+
+from pkg_resources import working_set
 from boggle import Boggle
-from flask import Flask, request, session, redirect , render_template
+from flask import Flask, request, session, redirect , render_template, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -18,15 +22,37 @@ def index():
 @app.route('/start', methods=["POST"])
 def start_game():
     session['board'] =[]
+    session['score'] = 0
     return redirect('/board')
 
 @app.route('/board')
 def generate_board():
     board = boggle_game.make_board()
     session['board'] = board
+    session['score'] = 0
 
     print('*******SESSION*******')
     print(session['board'])
     print('*******SESSION*******')
     return render_template("board.html", board = board) 
 
+@app.route("/word_check")
+def word_validity():
+    guess = request.args["guess"]
+    board = session['board']
+    # print('**************')
+    # print(guess)
+    # print('**************')
+    #Pass in a board and word
+    res = boggle_game.check_valid_word(board, guess)
+    if res == "ok":
+        word_value = len(guess)
+        session['score'] += word_value
+        #word_value = session['score']
+
+    print('**************')
+    print(session['score'])
+    print('**************')
+    return jsonify({"response": res, "score": session['score']})
+
+#@app.route("/score_check", methods=)
